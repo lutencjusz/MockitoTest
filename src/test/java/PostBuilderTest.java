00b231e0e34f12258717a09cf2926ce74bbbef73
lutencjusz <lutencjusz@gmail.com>
2024-08-02 06:51:19 +0200

    dodanie adnotacji

--- src/test/java/PostBuilderTest.java
+++ src/test/java/PostBuilderTest.java
@@ -1,32 +1,44 @@
 import model.Post;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.junit.jupiter.MockitoSettings;
+import org.mockito.quality.Strictness;
 import service.PostService;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+@MockitoSettings(strictness = Strictness.WARN)
+@ExtendWith(MockitoExtension.class)
 public class PostBuilderTest {
+    @Mock PostService mockPostService;
+    @InjectMocks PostBuilder postBuilder;
+
     @Test
     public void shouldCreatePostWithGivenData() {
-        PostService postService = new PostService();
-        PostBuilder postBuilder = new PostBuilder(postService);
         Post post = postBuilder.createPost("title", "userId", "1");
         assertEquals("title", post.getTitle());
         assertEquals("userId", post.getUserId());
         assertEquals("1", post.getId());
         System.out.println(post);
+        Mockito.verify(mockPostService, Mockito.never()).fetchPost(Mockito.anyString());
+        Mockito.verifyNoInteractions(mockPostService);
     }
 
     @Test
     public void shouldUseRandomPostData() {
+
         final String TITLE = "mock title";
         final String USER_ID = "mock userId";
 
-        PostService mockPostService = Mockito.mock(PostService.class);
         Post mockPost = new Post();
         mockPost.setTitle(TITLE);
         mockPost.setUserId(USER_ID);
         Mockito.when(mockPostService.fetchPost("1")).thenReturn(mockPost);
+        Mockito.when(mockPostService.fetchPost("2")).thenReturn(mockPost);
 
         PostBuilder postBuilder = new PostBuilder(mockPostService);
 
